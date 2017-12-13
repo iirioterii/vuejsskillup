fe1f066977804e46fd21415cc2863d5bfe18acac
yuriyreva <yuriyreva@gmail.com>
2017-12-13 16:10:59 +0200

    Add SCSS loader to webpack config

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'My',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
