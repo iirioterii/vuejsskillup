abb358cb4d40b404aedf3ad771f92b432649e5d4
Ekaterina Vasilchenko <ekaterina.vasilchenko@nixsolutions.com>
2017-12-14 15:57:29 +0200

    added base css structure

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
       </router-link>
       <nav class="nav">
         <div class="nav-list">
-          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/" class="nav-link"><i class="fa fa-home nav-icon"></i></router-link>
           <router-link to="/articles" class="nav-link">Articles</router-link>
-          <router-link to="/login" class="nav-link"><i class="fa fa-sign-out"></i></router-link>
+          <router-link to="/login" class="nav-link"><i class="fa fa-sign-out nav-icon"></i></router-link>
         </div>
       </nav>
     </header>
@@ -32,54 +32,5 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  .header {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background: #fff;
-    box-shadow: 0 0 17.3px 0.7px rgba(0, 0, 0, 0.16);
-    color: #414141;
-  }
-  .logo {
-    padding: 10px;
-  }
-  .logo-img {
-    display: block;
-    height: 40px;
-  }
-  .nav {
-    display: block;
-    height: 100%;
-  }
-  .nav-list {
-    display: flex;
-  }
-  .nav-link {
-    padding: 10px;
-    height: 100%;
-    line-height: 40px;
-    display: block;
-    transition: .3s ease;
-  }
-  .nav-link:hover {
-    background-color: #e2e2e2;
-  }
-  .router-link-exact-active {
-    color: #41b883;
-    cursor: default;
-  }
-  .router-link-exact-active:hover {
-    background-color: transparent;
   }
 </style>
